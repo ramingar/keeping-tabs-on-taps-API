34f9f5688788f83a5f8a758581b06bdb5d3132fe
Rafael Minguet <rminguet@gmail.com>
2017-09-15 23:47:10 +0200

    Skeleton and configuration

---
+++ tests/my-component.test.js
@@ -0,0 +1,11 @@
+import test from 'tape';
+
+test('-------- Component: ...', (assert) => {
+    const message = '...';
+    const expected = 1;
+    const actual = 1;
+
+    assert.equal(actual, expected, message);
+
+    assert.end();
+});
