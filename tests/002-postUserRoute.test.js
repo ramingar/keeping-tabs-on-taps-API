bdc32f21aa44f4cd508a5a2b1d152a475ba4cfea
Rafael Minguet <rminguet@gmail.com>
2017-09-18 18:17:57 +0200

    Refactor tests

--- tests/002-postUserRoute.test.js
+++ tests/002-postUserRoute.test.js
@@ -2,27 +2,6 @@ import test from 'tape';
 import request from 'supertest';
 import app from '../src/index';
 
-test('-------- Controller: GET /', (assert) => {
-    const url = '/';
-    const message = 'Status must be 200 and response must match with the expected simple message';
-    const responsePayloadExpected = {message: 'Server up!!'};
-    const statusCodeExpected = 200;
-
-    request(app)
-        .get(url)
-        .expect(statusCodeExpected)
-        .expect('Content-Type', /json/)
-        .then(
-            (res) => {
-                assert.deepEqual(res.body, responsePayloadExpected, message);
-                assert.end();
-            }, (err) => {
-                assert.fail(err.message);
-                assert.end();
-            }
-        );
-});
-
 test('-------- Controller: POST /user (400 - Bad request - No password)', (assert) => {
     const url = '/user';
     const message = 'Status must be 400 if pass is empty';
