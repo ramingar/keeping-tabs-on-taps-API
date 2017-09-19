4d6d2b9eb0f9117a8904198586c74a8c89ce43d8
Rafael Minguet <rminguet@gmail.com>
2017-09-20 00:53:57 +0200

    GET /user/:id (with authorization)

--- tests/004-getUserByIdRoute.test.js
+++ tests/004-getUserByIdRoute.test.js
@@ -3,7 +3,8 @@ import request from 'supertest';
 import app from '../src/index';
 
 test('-------- Controller: GET /user/:id', (assert) => {
-    const url = '/user';
+    const urlUser = '/user';
+    const urlLogin = '/login';
     const messageExpectedStatusCode = 'Status must be 200 when you are retrieving an existing user';
     const messageExpectedName = 'The name must match with the expected name';
     const messageExpectedEmail = 'The email must match with the expected email';
@@ -15,20 +16,34 @@ test('-------- Controller: GET /user/:id', (assert) => {
         pass: '1111'
     };
 
+    let idUser = null;
+
     request(app)
-        .post(url)
+        .post(urlUser)
         .send(user)
         .then(
             (res) => {
+                idUser = res.body._id;
                 request(app)
-                    .get(url + '/' + res.body._id)
-                    .expect(statusCodeExpected)
+                    .post(urlLogin)
+                    .send({email: user.email, pass: user.pass})
                     .then(
-                        () => {
-                            assert.pass(messageExpectedStatusCode);
-                            assert.equal(res.body.name, user.name, messageExpectedName);
-                            assert.equal(res.body.email, user.email, messageExpectedEmail);
-                            assert.end();
+                        (res) => {
+                            request(app)
+                                .get(urlUser + '/' + idUser)
+                                .set('Authorization', 'Bearer ' + res.body.token)
+                                .expect(statusCodeExpected)
+                                .then(
+                                    (res) => {
+                                        assert.pass(messageExpectedStatusCode);
+                                        assert.equal(res.body.name, user.name, messageExpectedName);
+                                        assert.equal(res.body.email, user.email, messageExpectedEmail);
+                                        assert.end();
+                                    }, (err) => {
+                                        assert.fail(err.message);
+                                        assert.end();
+                                    }
+                                );
                         }, (err) => {
                             assert.fail(err.message);
                             assert.end();
