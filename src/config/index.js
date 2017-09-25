95a520f97838e50a3f0d4b9dee676fc91aaf14ed
Rafael Minguet <rminguet@gmail.com>
2017-09-25 23:42:42 +0200

    Configuration for Travis

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 // In production environment, app runs in Heroku and it won't use config files. Instead, it will use environment vars.
-// So, only if we are in other environment than production, we import the config files.
+// Same happens in test environment. Travis will use environment vars as well.
+// So, only if we are in other environment than production and test, we import the config files.
 // Because of that condition we can't use ES6 imports.
 const config = (app) => {
 
@@ -7,14 +8,15 @@ const config = (app) => {
     let configDev = {};
     let configTest = {};
 
-    if ('production' !== app.get('env')) {
+    if ('development' === app.get('env') || 'test_local' === app.get('env')) {
         configDev = require('./development').default;
         configTest = require('./test').default;
+        configProd = require('./production').default;
     }
 
     return 'production' === app.get('env') ?
         configProd :
-        'test' === app.get('env') ? configTest : configDev;
+        'test' === app.get('env') || 'test_local' === app.get('env') ? configTest : configDev;
 
 };
 
