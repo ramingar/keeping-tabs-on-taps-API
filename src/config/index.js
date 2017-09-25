8fb7ed17d24d2adcdc2929542037511f42444a76
Rafael Minguet <rminguet@gmail.com>
2017-09-25 20:41:05 +0200

    Heroku will use environment vars instead of config files

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,21 @@
-import configProd from './production';
-import configDev from './development';
-import configTest from './test';
+// In production environment, app runs in Heroku and it won't use config files. Instead, it will use environment vars.
+// So, only if we are in other environment than production, we import the config files.
+// Because of that condition we can't use ES6 imports.
+const config = (app) => {
 
-const config = (app) => app.get('env') === 'production' ?
-    configProd :
-    app.get('env') === 'test' ? configTest : configDev;
+    let configProd = {};
+    let configDev = {};
+    let configTest = {};
+
+    if ('production' !== app.get('env')) {
+        configDev = require('./development').default;
+        configTest = require('./test').default;
+    }
+
+    return 'production' === app.get('env') ?
+        configProd :
+        'test' === app.get('env') ? configTest : configDev;
+
+};
 
 export default config;
