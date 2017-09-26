6b0e50b979c2e8798996169fb8a657421525a759
Rafael Minguet <rminguet@gmail.com>
2017-09-26 15:16:51 +0200

    Promise handler in one single line

--- src/db.js
+++ src/db.js
@@ -2,11 +2,9 @@ import mongoose from 'mongoose';
 
 const connect = (config) => {
     mongoose.Promise = global.Promise;
-    const promise = mongoose.connect(process.env.APP_MONGODB || config.mongodb, {
+    return mongoose.connect(process.env.APP_MONGODB || config.mongodb, {
         useMongoClient: true
-    });
-
-    return promise.then((db) => db);
+    }).then((db) => db);
 };
 
 const disconnect = () => {
